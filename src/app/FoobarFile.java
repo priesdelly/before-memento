5acbea2f879653b8e25f47dc1bf6991f3c18bb9e
priesdelly <priesdelly@gmail.com>
2023-03-02 17:30:13 +0700

    git init

---
+++ src/app/FoobarFile.java
@@ -0,0 +1,50 @@
+package app;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FoobarFile {
+
+    private String content;
+
+    private final File fileObj;
+    private static final String FILE_DIR = "src/files";
+
+    public FoobarFile(String fileName) throws IOException {
+        this.content = "";
+
+        var pathDir = Paths.get(FILE_DIR);
+        if (!pathDir.toFile().exists()) {
+            Files.createDirectories(pathDir);
+        }
+
+        this.fileObj = new File(FILE_DIR, fileName);
+        if (!this.fileObj.exists()) {
+            var valid = this.fileObj.createNewFile();
+            if (!valid) {
+                throw new IOException("Can't create file");
+            }
+        }
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void save() {
+        try (var fileWriter = new FileWriter(this.fileObj.toString());
+             var bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write(this.content);
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+}
---
+++ src/app/MainApp.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.io.IOException;
+
+public class MainApp {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Start Application");
+
+
+        FoobarFile file = new FoobarFile("content.txt");
+
+        file.setContent("This is the first version of the file.");
+        file.save();
+
+        file.setContent("This is the second version of the file.");
+        file.save();
+
+//        file.undo();
+
+        System.out.println(file.getContent());
+    }
+}
